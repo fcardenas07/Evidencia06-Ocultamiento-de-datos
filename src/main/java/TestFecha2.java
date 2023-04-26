28f8f5a3e94836e7118be6f0e65e3f46a87ad03d
fcardenas07 <f.cardenasreyes.94@gmail.com>
2023-04-26 10:02:26 -0400

    Se añade clase TestFecha2

---
+++ src/main/java/TestFecha2.java
@@ -0,0 +1,11 @@
+import com.date.TodaysDate;
+
+public class TestFecha2 {
+    public static void main(String[] args) {
+        TodaysDate hoy = new TodaysDate();
+
+        hoy.printDateAndTime();
+
+        hoy.time();
+    }
+}
